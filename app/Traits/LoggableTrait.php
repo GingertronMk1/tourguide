fc44286b0a9237fcb8796c8f2e9595bd3b3e04bf
Jack Ellis <jackellis1504@gmail.com>
2023-04-26 10:24:21 +0100

    Creation and updating

--- app/Traits/LoggableTrait.php
+++ app/Traits/LoggableTrait.php
@@ -8,16 +8,28 @@ use Illuminate\Support\Facades\Auth;
 
 trait LoggableTrait {
 
-    public static function bootLoggable(): void
+    public static function bootLoggableTrait(): void
     {
         static::created(function(mixed $loggable) {
-             $log = new ActivityLog();
-             $log->user_id = Auth::user()?->id;
-             $log->loggable_type = get_class($loggable);
-             $log->loggable_id = $loggable->id;
-             $log->changed_data = $loggable->toArray();
-             $log->type = ActivityLog::TYPE_CREATED;
-             $log->save();
+            $log = new ActivityLog();
+            $log->user_id = Auth::user()?->id;
+            $log->loggable_type = get_class($loggable);
+            $log->loggable_id = $loggable->id;
+            $log->old_data = [];
+            $log->new_data = $loggable->toArray();
+            $log->event_type = ActivityLog::TYPE_CREATED;
+            $log->save();
+        });
+
+        static::updated(function(mixed $loggable) {
+            $log = new ActivityLog();
+            $log->user_id = Auth::user()?->id;
+            $log->loggable_type = get_class($loggable);
+            $log->loggable_id = $loggable->id;
+            $log->old_data = $loggable->getOriginal();
+            $log->new_data = $loggable->toArray();
+            $log->event_type = ActivityLog::TYPE_CREATED;
+            $log->save();
         });
     }
 

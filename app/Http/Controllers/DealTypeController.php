c9590246d350e1e6209436f201610a7a19a36825
Jack Ellis <jackellis1504@gmail.com>
2023-04-26 10:51:00 +0100

    Add deal types model

---
+++ app/Http/Controllers/DealTypeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDealTypeRequest;
+use App\Http\Requests\UpdateDealTypeRequest;
+use App\Models\DealType;
+
+class DealTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDealTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DealType $dealType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DealType $dealType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDealTypeRequest $request, DealType $dealType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DealType $dealType)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_04_26_094636_create_deal_types_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\DealType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deal_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        $initial_types = [
+            'Hire',
+            'Split',
+            'Guarantee'
+        ];
+
+        foreach($initial_types as $type) {
+            DealType::create(['name' => $type]);
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deal_types');
+    }
+};

bfbd9f09dea331c1b642685fd0e37b26257eafbd
Jack Ellis <jackellis1504@gmail.com>
2023-04-26 19:17:29 +0100

    If there's a user do things

--- resources/js/Layouts/BaseLayout.vue
+++ resources/js/Layouts/BaseLayout.vue
@@ -1,11 +1,18 @@
 <script setup>
-import { ref } from "vue";
+import { Link } from "@inertiajs/vue3";
+
 </script>
 
 <template>
     <div class="flex flex-col">
-        <header class="p-4 bg-white border-b border-gray-100">
-            <slot name="header" />
+        <header class="p-4 bg-white border-b border-gray-100 flex flex-row">
+            <div class="flex-1">
+                <slot name="header" />
+            </div>
+            <div v-if="$page?.props?.auth?.user"></div>
+            <div v-else>
+                <Link :href="route('login')">Log In</Link>
+            </div>
         </header>
         <div class="p-4 flex-1 flex flex-col">
             <slot />

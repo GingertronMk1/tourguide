7e41b69b0772bccb9ca5c3edf96c45bdb5d8e2a5
Jack Ellis <jackellis1504@gmail.com>
2023-04-26 11:26:08 +0100

    Venues

---
+++ app/Models/Venue.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\LoggableTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Venue extends Model
+{
+    use HasFactory, LoggableTrait, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'notes',
+        'street_address',
+        'city',
+        'region_id',
+        'venue_type_id',
+        'maximum_stage_width',
+        'maximum_stage_depth',
+        'maximum_stage_height',
+        'maximum_seats',
+        'maximum_wheelchair_seats',
+        'number_of_dressing_rooms',
+        'backstage_wheelchair_access',
+    ];
+
+    protected $casts = [
+        'backstage_wheelchair_access' => 'boolean'
+    ];
+
+    public function getRegion(): BelongsTo
+    {
+        return $this->belongsTo(Region::class);
+    }
+
+    public function getArea(): BelongsTo
+    {
+        return $this->region->getArea();
+    }
+
+    public function getVenueType(): BelongsTo
+    {
+        return $this->belongsTo(VenueType::class);
+    }
+}
---
+++ database/migrations/2023_04_26_101953_create_venues_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Region;
+use App\Models\VenueType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('venues', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('notes')->nullable();
+            $table->text('street_address');
+            $table->string('city');
+            $table->foreignIdFor(Region::class)->constrained();
+            $table->foreignIdFor(VenueType::class)->constrained();
+            $table->unsignedBigInteger('maximum_stage_width');
+            $table->unsignedBigInteger('maximum_stage_depth');
+            $table->unsignedBigInteger('maximum_stage_height');
+            $table->unsignedBigInteger('maximum_seats');
+            $table->unsignedBigInteger('maximum_wheelchair_seats');
+            $table->unsignedBigInteger('number_of_dressing_rooms');
+            $table->boolean('backstage_wheelchair_access');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('venues');
+    }
+};

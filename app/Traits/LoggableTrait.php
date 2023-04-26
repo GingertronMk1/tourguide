2c512880df1f6ba9d2bb4657bbec5f1633039636
Jack Ellis <jackellis1504@gmail.com>
2023-04-26 10:25:40 +0100

    Add events

--- app/Traits/LoggableTrait.php
+++ app/Traits/LoggableTrait.php
@@ -6,11 +6,12 @@ use App\Models\ActivityLog;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Auth;
 
-trait LoggableTrait {
+trait LoggableTrait
+{
 
     public static function bootLoggableTrait(): void
     {
-        static::created(function(mixed $loggable) {
+        static::created(function (mixed $loggable) {
             $log = new ActivityLog();
             $log->user_id = Auth::user()?->id;
             $log->loggable_type = get_class($loggable);
@@ -21,14 +22,25 @@ trait LoggableTrait {
             $log->save();
         });
 
-        static::updated(function(mixed $loggable) {
+        static::updated(function (mixed $loggable) {
             $log = new ActivityLog();
             $log->user_id = Auth::user()?->id;
             $log->loggable_type = get_class($loggable);
             $log->loggable_id = $loggable->id;
             $log->old_data = $loggable->getOriginal();
             $log->new_data = $loggable->toArray();
-            $log->event_type = ActivityLog::TYPE_CREATED;
+            $log->event_type = ActivityLog::TYPE_UPDATED;
+            $log->save();
+        });
+
+        static::deleted(function (mixed $loggable) {
+            $log = new ActivityLog();
+            $log->user_id = Auth::user()?->id;
+            $log->loggable_type = get_class($loggable);
+            $log->loggable_id = $loggable->id;
+            $log->old_data = $loggable->getOriginal();
+            $log->new_data = [];
+            $log->event_type = ActivityLog::TYPE_DELETED;
             $log->save();
         });
     }

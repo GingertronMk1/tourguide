602491e66333a6bbb281405109eec996b49e63e4
Jack Ellis <jackellis1504@gmail.com>
2023-05-03 20:05:58 +0100

    OK seeding venues in a more or less reasonable fashion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,10 +9,30 @@ use App\Models\Area;
 use App\Models\DealType;
 use App\Models\User;
 use App\Models\VenueType;
+use Faker\Generator;
+use Illuminate\Container\Container;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+        /**
+     * The current Faker instance.
+     *
+     * @var \Faker\Generator
+     */
+    protected Generator $faker;
+
+    /**
+     * Create a new seeder instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->faker = Container::getInstance()->make(Generator::class);
+    }
+
     /**
      * Seed the application's database.
      */
@@ -42,14 +62,45 @@ class DatabaseSeeder extends Seeder
                 $dealTypes = DealType::all();
                 $venueTypes = VenueType::all();
 
-                $numberOfAccessEquipments = $accessEquipment->count();
-                $numberOfDealTypes = $dealTypes->count();
-                $numberOfVenueTypes = $venueTypes->count();
+                $accessEquipmentCount = $accessEquipment->count();
+                $dealTypeCount = $dealTypes->count();
+                $venueTypeCount = $venueTypes->count();
 
                 $accessEquipmentOffset = 0;
                 $accessEquipmentLimit = 0;
+                $dealTypeOffset = 0;
+                $dealTypeLimit = 0;
+                $venueTypeOffset = 0;
                 foreach ($area->regions as $region) {
+                    $accessEquipmentLimit = (++$accessEquipmentLimit) % $accessEquipmentCount;
+                    $dealTypeLimit = (++$dealTypeLimit) % $dealTypeCount;
+                    $venueTypeOffset = (++$venueTypeOffset) % $venueTypeCount;
+                    if ($accessEquipmentLimit === 0) {
+                        $accessEquipmentOffset = (++$accessEquipmentOffset) % $accessEquipmentCount;
+                    }
+                    if ($dealTypeLimit === 0) {
+                        $dealTypeOffset = (++$dealTypeOffset) % $dealTypeCount;
+                    }
 
+                    $stageDimension = ($dealTypeLimit + 1) * ($accessEquipmentLimit + 1) * 100;
+                    $seats = 100;
+                    echo "Creating venue for {$region->name}\n\n";
+                    $venue = $region->venues()->create([
+                        'name' => "{$region->name} Theatre",
+                        'venue_type_id' => $venueTypes->values()->get($venueTypeOffset)->id,
+                        'maximum_stage_width' => $stageDimension,
+                        'maximum_stage_depth' => $stageDimension,
+                        'maximum_stage_height' => $stageDimension,
+                        'maximum_seats' => $seats,
+                        'maximum_wheelchair_seats' => $seats / 10,
+                        'number_of_dressing_rooms' => $stageDimension / 10,
+                        'backstage_wheelchair_access' => $venueTypeOffset % 2,
+                        'street_address' => "1 Main Street\n{$region->name}\n{$region->area->name}",
+                        'city' => "{$region->name} City"
+                    ]);
+
+                    $venue->accessEquipment()->attach($accessEquipment->slice($accessEquipmentOffset, $accessEquipmentLimit)->pluck('id'));
+                    $venue->dealTypes()->attach($dealTypes->slice($dealTypeOffset, $dealTypeLimit)->pluck('id'));
                 }
             });
         }

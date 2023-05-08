11b15eeae89124970d8b62792fcf3beb84905c54
Jack Ellis <jackellis1504@gmail.com>
2023-05-08 17:35:01 +0100

    Add a handy-dandy action to create a resource

---
+++ app/Console/Commands/MakeResourcePages.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeResourcePages extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:make-resource-pages {resourceFolderName?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a set of resource pages under a given folder';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $path = $this->argument('resourceFolderName');
+        while (!is_string($path)) {
+            $path = $this->ask('What is the name of the resource?');
+        }
+
+        $filesToCreate = [
+            "Components" => ["EditForm"],
+            "Pages" => ['Create', 'Edit', 'Index', 'Show']
+        ];
+        foreach($filesToCreate as $dir => $fileNames) {
+            $dirPath = resource_path("js/{$dir}/{$path}");
+            foreach($fileNames as $fileName) {
+                if(!is_dir($dirPath)) {
+                    mkdir($dirPath, recursive: true);
+                }
+                file_put_contents(
+                    "{$dirPath}/{$fileName}.vue",
+<<<JS
+<script setup>
+
+</script>
+<template>
+</template>
+
+JS
+                );
+            }
+        }
+    }
+}

f8069b6789269e0eb127e03d1cbc8a71c9feb0f9
Jack Ellis <jackellis1504@gmail.com>
2023-05-15 11:00:36 +0100

    Update layout and such

--- resources/views/pdfs/venue.blade.php
+++ resources/views/pdfs/venue.blade.php
@@ -7,14 +7,24 @@
 <body>
     <h1>{{ $venue->name }}</h1>
 
-    <h2>Description</h2>
-    <p>{{ $venue->description }}</p>
-
-    <h2>Notes</h2>
-    <p>{{ $venue->notes }}</p>
-
     <h2>Some Statistics</h2>
-    <table border="1" cellspacing="0" cellpadding="10">
+    <table border="1" cellspacing="0" cellpadding="10" id="details-table">
+        <tr>
+            <td>Area</td>
+            <td>{{ $venue->area?->name ?? 'No Area' }}</td>
+        </tr>
+        <tr>
+            <td>Region</td>
+            <td>{{ $venue->region?->name ?? 'No Region' }}</td>
+        </tr>
+        <tr>
+            <td>City</td>
+            <td>{{ $venue->city }}</td>
+        </tr>
+        <tr>
+            <td>Street Address</td>
+            <td>{{ $venue->street_address }}</td>
+        </tr>
         <tr>
             <td>Stage Width</td>
             <td>{{ $venue->maximum_stage_width }}mm</td>
@@ -45,27 +55,53 @@
         </tr>
     </table>
 
-    <h2>Access Equipment</h2>
-    @if (count($venue->accessEquipment) > 0)
-        <ul>
-            @foreach ($venue->accessEquipment as $access)
-                <li>{{ $access->name }}: {{ $access->pivot->notes ?? 'No Notes' }}</li>
-            @endforeach
-        </ul>
-    @else
-        <p>This venue has no Access Equipment</p>
-    @endif
+    <section>
+        <h2>Description</h2>
+        <p>{{ $venue->description }}</p>
+    </section>
 
-    <h2>Deal Types</h2>
-    @if (count($venue->dealTypes) > 0)
-        <ul>
-            @foreach ($venue->dealTypes as $dealType)
-                <li>{{ $dealType->name }}: {{ $dealType->pivot->notes ?? 'No Notes' }}</li>
-            @endforeach
-        </ul>
-    @else
-        <p>This venue has no Deal Types available</p>
-    @endif
+    <section>
+        <h2>Notes</h2>
+        <p>{{ $venue->notes }}</p>
+    </section>
+
+    <section>
+        <h2>Access Equipment</h2>
+        @if (count($venue->accessEquipment) > 0)
+            <ul>
+                @foreach ($venue->accessEquipment as $access)
+                    <li>{{ $access->name }}: {{ $access->pivot->notes ?? 'No Notes' }}</li>
+                @endforeach
+            </ul>
+        @else
+            <p>This venue has no Access Equipment</p>
+        @endif
+    </section>
+
+    <section>
+        <h2>Deal Types</h2>
+        @if (count($venue->dealTypes) > 0)
+            <ul>
+                @foreach ($venue->dealTypes as $dealType)
+                    <li>{{ $dealType->name }}: {{ $dealType->pivot->notes ?? 'No Notes' }}</li>
+                @endforeach
+            </ul>
+        @else
+            <p>This venue has no Deal Types available</p>
+        @endif
+    </section>
+
+    <h2 id="photos-header">Photos</h2>
+    @forelse ($venue->photoAssets as $photoAsset)
+        <div class="photo">
+            <h4 class="photo-header">
+                {{ $photoAsset->title }} | {{ $photoAsset->type }}
+            </h4>
+            <img src="{{ $photoAsset->fileURL }}" alt="{{ $photoAsset->title }}" width="600" />
+        </div>
+    @empty
+        <h2>No photos for this venue</h2>
+    @endforelse
 </body>
 
 <style>
@@ -77,8 +113,28 @@
         width: 100%;
     }
 
-    td:first-of-type {
-        width: 75%;
+    #details-table td:first-of-type {
+        width: 35%;
+    }
+
+    img {
+        object-fit: scale-down;
+    }
+
+    #photos-header {
+        page-break-before: always;
+    }
+
+    .photo {
+        page-break-inside: avoid;
+    }
+
+    .photo+.photo {
+        margin-top: 10px;
+    }
+
+    section {
+        page-break-inside: avoid;
     }
 </style>
 

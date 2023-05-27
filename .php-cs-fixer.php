511ed96475a29cea96ceae68d45e2374e649eb75
Jack Ellis <jackellis1504@gmail.com>
2023-05-27 17:33:38 +0100

    This should have it matching my gitignore

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,15 +1,42 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
-    ->exclude('vendor')
-    ->exclude('node_modules')
+use PhpCsFixer\Finder;
+
+$gitIgnoreFiles = Finder::create()
+    ->name('/^.gitignore$/')
+    ->ignoreVCS(false)
+    ->ignoreDotFiles(false)
     ->in(__DIR__)
 ;
+
+$ignores = [];
+
+foreach ($gitIgnoreFiles as $key => $file) {
+    if (str_ends_with($key, '.gitignore')) {
+        foreach(explode(PHP_EOL, $file->getContents()) as $ignore) {
+            if (strlen($ignore) > 0) {
+                $ignores[] = $ignore;
+            }
+        }
+    }
+}
+
+$finder = Finder::create()->in(__DIR__);
+
+foreach($ignores as $ignore) {
+    if (str_starts_with($ignore, '/')) {
+        $finder->exclude($ignore);
+    } else {
+        $finder->notName($ignore);
+    }
+}
 
 $config = new PhpCsFixer\Config();
 return $config->setRules([
         '@PhpCsFixer' => true,
         'yoda_style' => false,
+        'declare_strict_types' => true,
     ])
+    ->setRiskyAllowed(true)
     ->setFinder($finder)
 ;
